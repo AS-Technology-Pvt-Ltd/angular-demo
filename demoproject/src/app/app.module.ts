cd2ce54d31d7646924090e86d3173e994fbc0df9
saching-sdei <saching.ss@smartdatainc.net>
2020-05-15 19:41:04 +0530

    header,slider,about,help-homework

--- demoproject/src/app/app.module.ts
+++ demoproject/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SliderComponent } from './slider/slider.component';
+import { AboutComponent } from './about/about.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, SliderComponent, AboutComponent, HelpComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
