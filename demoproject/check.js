03009c3e93170542e6a221f1a4a41fb97b3ac2c9
saching-sdei <saching.ss@smartdatainc.net>
2020-05-16 14:35:58 +0530

    basic JS

---
+++ demoproject/check.js
@@ -0,0 +1,22 @@
+let data = ["A", "B", "C", "D", "E"];
+
+var ages = [3, 10, 18, 20];
+
+// let x = [];
+
+// for (let i = 0; i < data.length; i++) {
+//   x.push(data[i]);
+// }
+
+// console.log(x);
+
+let x = ages.find((item, i) => {
+  return item > 6;
+});
+
+// let y = ages.findIndex(function (item, i) {
+//   // console.log(item);
+//   return item > 3;
+// });
+
+console.log(x);
