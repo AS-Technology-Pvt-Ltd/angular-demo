84dee198a98deeef1d604e142eb571441ff26e0a
saching-sdei <saching.ss@smartdatainc.net>
2020-05-16 14:23:03 +0530

    string interpolation,property binding

--- demoproject/src/app/app.module.ts
+++ demoproject/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SliderComponent } from './slider/slider.component';
-import { AboutComponent } from './about/about.component';
 import { HelpComponent } from './help/help.component';
+import { AboutComponent } from './about/about.component';
+import { LogicComponent } from './logic/logic.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SliderComponent, AboutComponent, HelpComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    HelpComponent,
+    AboutComponent,
+    LogicComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ demoproject/src/app/logic/logic.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logic',
+  templateUrl: './logic.component.html',
+  styleUrls: ['./logic.component.css'],
+})
+export class LogicComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  name: string = 'PASHJ';
+
+  sum(a, b) {
+    return a + b;
+  }
+  userStatus: boolean = true;
+
+  getUserName() {
+    return 'username is' + this.name;
+  }
+}
